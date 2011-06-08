d39e3c5ca5ddc6089b493e501d446393d738f216
Jaza <jazepstein@gmail.com>
2011-06-09 01:16:51 +0200

    show favicon

--- project/fotojazz/views.py
+++ project/fotojazz/views.py
@@ -1,8 +1,18 @@
+import os
+
+from flask import current_app as app
 from flask import Module
 from flask import render_template
+from flask import send_from_directory
+
 
 mod = Module(__name__, 'fotojazz')
 
 @mod.route('/')
 def home():
     return render_template('home.html')
+
+@mod.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico')
