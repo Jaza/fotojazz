5a52967df5484c9c3211d9c0beb9f3c985dc275d
Jaza <jazepstein@gmail.com>
2011-06-26 18:56:54 -0300

    fix the 'Exif.Image.DateTime' value as part of datemodified process.

--- project/fotojazz/datemodified.py
+++ project/fotojazz/datemodified.py
@@ -20,6 +20,8 @@ class DateModifiedProcess(FotoJazzProcess):
             metadata = pyexiv2.ImageMetadata(filename)
             metadata.read()
             date_taken = metadata['Exif.Photo.DateTimeOriginal'].value
+            metadata['Exif.Image.DateTime'].value = date_taken
+            metadata.write()
             date_taken_timestamp = int(mktime(date_taken.timetuple()))
             utime(filename, (date_taken_timestamp, date_taken_timestamp))
             self.files_processed_count += 1
