5a9cfb8b0355d3245c8aea3fefa00c3a64fe5119
Jaza <jazepstein@gmail.com>
2011-06-29 12:40:03 -0300

    reduce line widths in fotojazzprocess.py

--- project/fotojazz/fotojazzprocess.py
+++ project/fotojazz/fotojazzprocess.py
@@ -8,7 +8,10 @@ from time import sleep
 
 
 class FotoJazzProcess(Thread):
-    """Parent / example class for running threaded FotoJazz processes. You should use this as a base class if you want to process a directory full of files, in batch, within a thread, and you want to report on the progress of the thread."""
+    """Parent / example class for running threaded FotoJazz processes.
+    You should use this as a base class if you want to process a
+    directory full of files, in batch, within a thread, and you want to
+    report on the progress of the thread."""
     
     filenames_str = ''
     filebrowse_path = ''
@@ -22,7 +25,9 @@ class FotoJazzProcess(Thread):
     files_processed_count = 0
     
     def __init__(self, *args, **kwargs):
-        """When initialising this class, you can pass in either a list of filenames (first param), or a string of space-delimited filenames (second param). No need to pass in both."""
+        """When initialising this class, you can pass in either a list
+        of filenames (first param), or a string of space-delimited
+        filenames (second param). No need to pass in both."""
         Thread.__init__(self)
         if 'filenames' in kwargs:
             self.filenames = kwargs['filenames']
@@ -33,7 +38,13 @@ class FotoJazzProcess(Thread):
         self.files_processed_count = 0
     
     def run(self):
-        """Iterates through the files in the specified directory. This example implementation just sleeps on each file - in subclass implementations, you should do some real processing on each file (e.g. re-orient the image, change date modified). You should also generally call self.prepare_filenames() at the start, and increment self.files_processed_count, in subclass implementations."""
+        """Iterates through the files in the specified directory. This
+        example implementation just sleeps on each file - in subclass 
+        implementations, you should do some real processing on each
+        file (e.g. re-orient the image, change date modified). You
+        should also generally call self.prepare_filenames() at the
+        start, and increment self.files_processed_count, in subclass
+        implementations."""
         self.prepare_filenames()
         
         for filename in self.filenames:
@@ -41,9 +52,12 @@ class FotoJazzProcess(Thread):
             self.files_processed_count += 1
     
     def prepare_filenames(self):
-        """Prepares the string and the list of filenames - this is always run before the thread begins."""
+        """Prepares the string and the list of filenames - this is
+        always run before the thread begins."""
         if self.filenames_str != '':
-            self.filenames = [x.strip() for x in self.filenames_str.split(' ') if x != '']
+            self.filenames = [x.strip()
+                              for x in self.filenames_str.split(' ')
+                              if x != '']
             self.total_file_count = len(self.filenames)
         if len(self.filenames):
             self.filenames.sort()
@@ -52,11 +66,15 @@ class FotoJazzProcess(Thread):
     
     def percent_done(self):
         """Gets the current percent done for the thread."""
-        return float(self.files_processed_count) / float(self.total_file_count) * 100.0
+        return float(self.files_processed_count) / \
+                     float(self.total_file_count) \
+               * 100.0
     
     def get_progress(self):
-        """Can be called at any time before, during or after thread execution, to get current progress."""
-        return '%d files (%.2f%%)' % (self.files_processed_count, self.percent_done())
+        """Can be called at any time before, during or after thread
+        execution, to get current progress."""
+        return '%d files (%.2f%%)' % (self.files_processed_count,
+                                      self.percent_done())
 
 
 class FotoJazzProcessShellRun():
@@ -82,19 +100,22 @@ class FotoJazzProcessShellRun():
         fjp = self.init_class(*args, **kwargs)
 
         print '%s threaded process beginning.' % fjp.__class__.__name__
-        print '%d files will be processed. Now beginning progress output.' % fjp.total_file_count
+        print '%d files will be processed. ' % fjp.total_file_count + \
+              'Now beginning progress output.' 
         print fjp.get_progress()
 
         fjp.start()
 
-        while fjp.is_alive() and fjp.files_processed_count < fjp.total_file_count:
+        while fjp.is_alive() and \
+              fjp.files_processed_count < fjp.total_file_count:
             sleep(1)
             if fjp.files_processed_count < fjp.total_file_count:
                 print fjp.get_progress()
 
         if fjp.files_processed_count == fjp.total_file_count:
             print fjp.get_progress()
-            print '%s threaded process complete. Now exiting.' % fjp.__class__.__name__
+            print '%s threaded process complete. Now exiting.' \
+                  % fjp.__class__.__name__
 
 
 if __name__ == '__main__':
